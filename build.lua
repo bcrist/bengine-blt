0d82c386b35ab6dc153eb45e9dcb6e17457d12b4
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:53:22 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,22 +1,15 @@
-module { name = 'blt',
-   projects = {
-      lib {
-         custom = {
-            outputs = { 'src/lexer.cpp' },
-            inputs = { 'meta/lexer.rl' },
-            name = 'ragel',
-            command = 'ragel $flags -o $out $in',
-            flags = '-s -C -L -G2',
-            generator = true,
-            restat = true
-         },
-         src = {
-            'src/*.cpp',
-            pch = 'src/pch.cpp'
-         },
-         preprocessor = {
-            'BE_BLT_IMPL'
-         }
-      }
+module 'blt' {
+   lib {
+      custom {
+         outputs = 'src/lexer.cpp',
+         inputs = 'meta/lexer.rl',
+         rule = 'ragel',
+         command = 'ragel $flags -o $out $in',
+         flags = '-s -C -L -G2',
+         generator = true,
+         restat = true
+      },
+      pch_src 'src/pch.cpp',
+      define 'BE_BLT_IMPL'
    }
 }
